f0c57a6ca3250cc317e4c5de954f5e6d9b1dab9f
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-09-09 00:38:00 +0700

    feat(workspace): rebuild Settings page with theme and language controls

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { initTheme } from './lib/theme'
+import { initLang } from './lib/lang'
 
 initTheme()
+initLang()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
